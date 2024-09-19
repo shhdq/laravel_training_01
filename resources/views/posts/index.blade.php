6c61b7c704c59c10f15c4fbdc3a4b324cb2eb561
shhdq <54862478+shhdq@users.noreply.github.com>
2024-09-19 19:33:04 +0300

    project init

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Posts</h1>
+
+    <a href="{{ route('posts.create') }}">Create Post</a>
+
+    @if (session('success'))
+        <div>
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @foreach ($posts as $post)
+        <div>
+            <h2>{{ $post->title }}</h2>
+            <p>{{ $post->content }}</p>
+            <a href="{{ route('posts.show', $post) }}">View</a>
+            <a href="{{ route('posts.edit', $post) }}">Edit</a>
+            <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                @csrf
+                @method('DELETE')
+    @endforeach
+
+    {{ $posts->links() }}
+@endsection
